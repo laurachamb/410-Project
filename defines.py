4be5b15444ae98a62e41d4fd759a87cbcd3368b5
laurachamb <91031382+laurachamb@users.noreply.github.com>
2021-11-16 13:28:05 -0500

    Update defines.py
    Updated to be for Impact Finance Center rather than the personal Facebook page

--- defines.py
+++ defines.py
@@ -4,15 +4,16 @@ import json
 def getCreds():
     creds = dict()
     #empty values should be generated/accessed through the facebook developer account
-    creds['access_token'] = ''
+    creds['user_access_token'] = ''
+    creds['page_access_token'] = ''
     creds['client_id'] = ''
     creds['client_secret'] = ''
-    creds['ig_username'] = ''
+    creds['ig_username'] = 'impactfinctr'
 
     creds['graph_domain'] = 'https://graph.facebook.com/'
-    creds['graph_version'] = 'v6.0'
+    creds['graph_version'] = 'v12.0'
     creds['endpoint_base'] = creds['graph_domain'] + creds['graph_version'] + '/'
     creds['debug'] = 'no'
-    creds['page_id'] = ''
+    creds['page_id'] = '1503119803264962'
 
     return creds
