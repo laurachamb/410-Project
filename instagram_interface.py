31405942657f3e788aadc532cfd1ad463d46bdcc
laurachamb <91031382+laurachamb@users.noreply.github.com>
2021-11-17 10:50:49 -0500

    Update instagram_interface.py
    Fixed after using the wrong Instagram id. pageId() still needs to be updated but is really only for initial use. Added some tests to test igFollowers() and igPostCount(). The commented out media() function is what I'll be working on next.

--- instagram_interface.py
+++ instagram_interface.py
@@ -8,51 +8,51 @@ def makeApiCall(url, endpointParams, debug='no'):
     response['endpoint_params'] = json.dumps(endpointParams, indent=4)
     response['json_data'] = json.loads(data.content)
  #printing this will help with visualizing json data
-   # response['json_data_pretty'] = json.dumps(response['json_data'], indent=4 )
+    response['json_data_pretty'] = json.dumps(response['json_data'], indent=4 )
 
     return response
 
 
-def pageId():
-    params = defines.getCreds()
-    endpointParams = dict()
-    endpointParams['access_token'] = params['access_token']
-    url = params['endpoint_base'] + 'me/accounts'
-    response = makeApiCall(url, endpointParams, params['debug'])
-    return response['json_data']['data'][0]['id']
-
-
+# needs to be updated
 def instagramPageId():
     params = defines.getCreds()
     endpointParams = dict()
-    endpointParams['access_token'] = params['access_token']
-    endpointParams['fields'] = 'instagram_business_account'
-
-    url = params['endpoint_base'] + str(pageId())
+    endpointParams['access_token'] = params['page_access_token']
+    url = url = params['endpoint_base'] + "/me/instagram_accounts"
     response = makeApiCall(url, endpointParams, params['debug'])
-    return response['json_data']['instagram_business_account']['id']
+    return response['json_data']['data'][0]['id']
 
 
 
 def igFollowers():
     params = defines.getCreds()
     endpointParams = dict()
-    endpointParams['access_token'] = params['access_token']
-    endpointParams['fields'] = 'business_discovery.username(' + params['ig_username'] + '){follower_count}'
-    endpointParams['access_token'] = params['access_token']
-
-    url = params['endpoint_base'] + str(instagramPageId())
+    endpointParams['fields'] = 'followed_by_count'
+    endpointParams['access_token'] = params['page_access_token']
+    url = params['endpoint_base'] + params['ig_page_id']
     response = makeApiCall(url, endpointParams, params['debug'])
-    return response['json_data']['business_discovery']['follower_count']
+    return response['json_data']['followed_by_count']
 
 
-def ifPostCount():
+def igPostCount():
     params = defines.getCreds()
     endpointParams = dict()
-    endpointParams['access_token'] = params['access_token']
-    endpointParams['fields'] = 'business_discovery.username(' + params['ig_username'] + '){media_count}'
-    endpointParams['access_token'] = params['access_token']
-    url = params['endpoint_base'] + '/'+ str(instagramPageId())
+    endpointParams['access_token'] = params['page_access_token']
+    endpointParams['fields'] = 'media_count'
+    url = params['endpoint_base'] + params['ig_page_id']
     response = makeApiCall(url, endpointParams, params['debug'])
-    return response['json_data']['business_discovery']['media_count']
-    return response['json_data']['business_discovery']['media_count']
+    return response['json_data']['media_count']
+
+
+#def media():
+#    params = defines.getCreds()
+#    endpointParams = dict()
+#   endpointParams['access_token'] = params['page_access_token']
+
+id = instagramPageId()
+print('Page id: ' + str(id))
+followers = igFollowers()
+print('Followers: ' + str(followers))
+posts = igPostCount()
+print('Number of Posts: ' + str(posts))
+
