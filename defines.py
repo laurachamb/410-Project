f9fe5b2811d894797792cc7bf54393f4ec8ac3cc
laurachamb <91031382+laurachamb@users.noreply.github.com>
2021-11-17 10:47:24 -0500

    Update defines.py
    Updated with Instagram username and business account id

--- defines.py
+++ defines.py
@@ -6,8 +6,7 @@ def getCreds():
     #empty values should be generated/accessed through the facebook developer account
     creds['user_access_token'] = ''
     creds['page_access_token'] = ''
-    creds['client_id'] = ''
-    creds['client_secret'] = ''
+    creds['client_secret'] = '●●●●●●●●'
     creds['ig_username'] = 'impactfinctr'
 
     creds['graph_domain'] = 'https://graph.facebook.com/'
@@ -15,5 +14,5 @@ def getCreds():
     creds['endpoint_base'] = creds['graph_domain'] + creds['graph_version'] + '/'
     creds['debug'] = 'no'
     creds['page_id'] = '1503119803264962'
-
+    creds['ig_page_id'] = '17841404219581713'
     return creds
