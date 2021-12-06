a428330b757bf2b4489648b88c8ec97726cd273f
laurachamb <91031382+laurachamb@users.noreply.github.com>
2021-12-06 17:13:30 -0500

    Update defines.py
    Updated with long lived access tokens

--- defines.py
+++ defines.py
@@ -4,9 +4,10 @@ import json
 def getCreds():
     creds = dict()
     #empty values should be generated/accessed through the facebook developer account
-    creds['user_access_token'] = ''
-    creds['page_access_token'] = ''
-    creds['client_secret'] = '●●●●●●●●'
+    creds['user_access_token'] = 'EAAI3fQapZATcBANavKcIGQcut5dH45y01mMNvdrmS2go96qEP8PfXdKcUqChnO57g9UwKoPFZC8qkv5I1KZB8zBiAdg0MbVVWgUfXZBoDefjaZA6uD9PbgMCc48FMvA3eFpLEyWbZAH3yJ2AZARIH5yVJYmy9ZAs9DgRJQ96nduHkQZDZD'
+    creds['page_access_token'] = 'EAAI3fQapZATcBALvcDr7oIZAOF7DcNZCA3xIVGXdfewUzHNklltFY78k08IoHolZAR0B0TIXtO0yurHL3epDesxrdmjuh44y652fACrBYYjOgm9N1ZAqCd4qp5ZAPJMoXtT7xKgTv1rtuaFKZAp9RpxwUjakrYrRLMu7hEB3AJikoebOSPJHVLx'
+    creds['client_id'] = '623960075625783'
+    creds['client_secret'] = ''
     creds['ig_username'] = 'impactfinctr'
 
     creds['graph_domain'] = 'https://graph.facebook.com/'
